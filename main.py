f7ff0232745493bf881bd90fa7b66bf34812aa86
Tojo Chacko <joey85in@gmail.com>
2025-07-01 21:28:25 +0530

    Initial commit with a some scaffold code

---
+++ main.py
@@ -0,0 +1,7 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("Some question about the data should go here")
+print(response)
